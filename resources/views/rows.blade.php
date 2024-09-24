4d13304eea68e79dd089b9bd05c637d85fda45e7
Abdalrhman-Jabri <abdalrhmanjabri08@gmail.com>
2024-09-24 18:18:08 +0300

    First attempt

--- resources/views/rows.blade.php
+++ resources/views/rows.blade.php
@@ -19,13 +19,13 @@
                             </tr>
                         </thead>
                         <tbody>
-                            @foreach ($users as $user)
+                            @foreach ($users as $index => $user)
                                 {{-- Task: only every second row should have "bg-red-100" --}}
-                                <tr class="bg-red-100">
-                                    <td>{{-- Task: add row number here: 1, 2, etc. --}}</td>
+                                <tr @class(['bg-red-100' => $index == 1])>
+                                    <td>{{ $index }}</td>
                                     <td>{{ $user->name }}</td>
                                     {{-- Task: only the FIRST row should have email with "font-bold" --}}
-                                    <td class="font-bold">{{ $user->email }}</td>
+                                    <td @class(['font-bold' => $index == 0])>{{ $user->email }}</td>
                                     <td>{{ $user->created_at }}</td>
                                 </tr>
                             @endforeach
